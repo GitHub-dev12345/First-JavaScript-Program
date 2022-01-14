996539d90cee3fcb8c8128eab1556578081cf62c
Dev <devrathore986@gmail.com>
2022-01-14 17:34:01 +0530

    Add a New Js Program for practice purpose

--- scrip.js
+++ scrip.js
@@ -1,15 +1,23 @@
-let namasteBtn = document.querySelector("button");
-namasteBtn.addEventListener('click', ShowMsg);
+/* This Code And Js Work on input program  */
+// let namasteBtn = document.querySelector("button");
+// namasteBtn.addEventListener('click', ShowMsg);
 
-function ShowMsg(){
-    let name = prompt('Enter Your Name');
-    namasteBtn.textContent = "Roll No. 1:" + name;
+// function ShowMsg(){
+//     let name = prompt('Enter Your Name');
+//     namasteBtn.textContent = "Roll No. 1:" + name;
+// }
+
+
+/* This Program Write For practice Purpose */
+let namasteBtn = document.querySelector('button');
+namasteBtn.addEventListener('click', showMsg);
+
+function showMsg(){
+    alert("Hello Mrs. Dev Rathore");
 }
 
 
-
 /* This is a On-CLick & ALert Function  */
-
 // let namasteBtn = document.querySelector('button');
 // namasteBtn.addEventListener('click', showMsg);
 
