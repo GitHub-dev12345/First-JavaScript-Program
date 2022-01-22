c2f4fdb9192e92bf5cbc7e07b063fc1901c3e201
Dev <devrathore986@gmail.com>
2022-01-22 17:40:23 +0530

    Some Js Code Add & Delete

--- scrip.js
+++ scrip.js
@@ -5,6 +5,20 @@ namasteBtn.addEventListener('click', ShowMsg);
 function ShowMsg(){
   let name = prompt('Enter Your Name');
   namasteBtn.textContent = "Roll No. 1:" + name;
+
+  var time = prompt("Hey Whats the time:")
+  if (time>5 && time<12){
+      alert("Good Morning");
+  }
+  else if(time>12 && time<17){
+      alert("Good AfterNoon");
+  }
+  else if(time>17 && time<21){
+      alert("Good Evening");
+  }
+  else{
+      alert("Good Night");
+  }
 }
 
 
