1dd4ce114ff730f32184c6e7604de5350abd1444
Dev <devrathore986@gmail.com>
2022-02-27 15:36:46 +0530

    Change the JS Code

--- scrip.js
+++ scrip.js
@@ -1,36 +1,14 @@
-/* This Code And Js Work on input program  */
-let namasteBtn = document.querySelector("button");
-namasteBtn.addEventListener('click', ShowMsg);
-
-function ShowMsg(){
-  let name = prompt('Enter Your Name');
-  namasteBtn.textContent = "Roll No. 1:" + name;
-
-  var time = prompt("Hey Whats the time:")
-  if (time>5 && time<12){
-      alert("Good Morning");
-  }
-  else if(time>12 && time<17){
-      alert("Good AfterNoon");
-  }
-  else if(time>17 && time<21){
-      alert("Good Evening");
-  }
-  else{
-      alert("Good Night");
-  }
+// First Program ShowMsg 
+let firstBtn = document.getElementById('firstBtn');
+firstBtn.addEventListener('click', showMsg);
+function showMsg(){
+    alert('Hello World I am Dev Rathore, The first program in this planet Earth');
 }
 
-
-/* This Program Write For practice Purpose */
-//let namasteBtn = document.querySelector('button');
-//namasteBtn.addEventListener('click', showMsg);
-
-
-/* This is a On-CLick & ALert Function  */
-// let namasteBtn = document.querySelector('button');
-// namasteBtn.addEventListener('click', showMsg);
-
-// function showMsg(){
-//     alert("Hello World");
-// }
+// Second Program inpuMsg
+let secondBtn = document.getElementById('secondBtn');
+secondBtn.addEventListener('click', inpuMsg);
+function inpuMsg(){
+    let name = prompt('Enter Your Name');
+    secondBtn.textContent = name;
+}
